8de28d0cfe09ce0c6a7798096e815e6c8118ef9c
innicovation21 <95610543+innicovation21@users.noreply.github.com>
2023-07-27 06:53:13 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,23 +1,29 @@
-// ignore_for_file: use_key_in_widget_constructors
-
 import 'package:flutter/material.dart';
-import 'package:login_localstorage/screens/loginscreen.dart';
+import 'package:login_storage_database/screens/screen1.dart';
 import 'package:sizer/sizer.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
+    // Instanzieren des Sizer-Packages
     return Sizer(
       builder: (context, orientation, deviceType) {
         return MaterialApp(
+          // ausblenden des debug-banners
           debugShowCheckedModeBanner: false,
-          title: 'Sizer',
-          theme: ThemeData.light(),
-          home: LoginScreen(),
+          title: 'future & local storage',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          // Startscreen: Screen1()
+          home: const Screen1(),
         );
       },
     );
